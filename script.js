6cb510fffb7b53357a31ec4196637dcd99fc0935
Dr B Bony <rolandbricebony@gmail.com>
2023-10-31 20:19:10 +0100

    test push git

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-let myHeading = document.queryselector('h1');
-myHeading.TextContent = 'Bonjour le monde !';
+let button = document.querySelector('#selctionButton')
+let menu = document.querySelector('.optionCustomSelect')
+let icon = document.querySelector('.selectionIcon')
+
+
+button.addEventListener('click',()=>{
+    if(menu.classList.contains('desactived')){
+        menu.classList.remove('desactived')
+        menu.classList.add('activated')
+        icon.style = 'transform: rotate(180deg)'
+    }else{
+        menu.classList.remove('activated')
+        menu.classList.add('desactived')
+        icon.style = 'transform: rotate(0deg)'
+    }
+})
+
+
